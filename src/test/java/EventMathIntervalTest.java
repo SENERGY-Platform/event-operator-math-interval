ade8ad02f97d2e4067baee87b471ab2b689cffb6
Ingo <roessner@infai.org>
2020-02-24 16:31:39 +0100

    fix trigger msg

--- src/test/java/EventMathIntervalTest.java
+++ src/test/java/EventMathIntervalTest.java
@@ -53,12 +53,12 @@ public class EventMathIntervalTest {
             try {
                 JSONObject jsonObject = (JSONObject)jsonParser.parse(new InputStreamReader(inputStream, "UTF-8"));
                 if(
-                        jsonObject.containsKey("localVariables")
-                        && ((JSONObject)jsonObject.get("localVariables")).containsKey("event")
-                        && ((JSONObject)((JSONObject)jsonObject.get("localVariables")).get("event")).containsKey("value")
+                        jsonObject.containsKey("processVariablesLocal")
+                        && ((JSONObject)jsonObject.get("processVariablesLocal")).containsKey("event")
+                        && ((JSONObject)((JSONObject)jsonObject.get("processVariablesLocal")).get("event")).containsKey("value")
                 ){
                     EventMathIntervalTest.called = true;
-                    EventMathIntervalTest.processVariable = ((JSONObject)((JSONObject)jsonObject.get("localVariables")).get("event")).get("value");
+                    EventMathIntervalTest.processVariable = ((JSONObject)((JSONObject)jsonObject.get("processVariablesLocal")).get("event")).get("value");
                 }
             } catch (Exception e) {
                 e.printStackTrace();
