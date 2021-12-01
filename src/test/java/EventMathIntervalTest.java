1e22e6f746694eb694ce53e54eb5e0a722b90f20
Ingo <roessner@infai.org>
2021-12-01 09:57:46 +0100

    implement user-tokens for event-trigger call

--- src/test/java/EventMathIntervalTest.java
+++ src/test/java/EventMathIntervalTest.java
@@ -72,7 +72,7 @@ public class EventMathIntervalTest {
         });
         HttpServer converterServer = ConverterServerMock.create("/inCharacteristic/outCharacteristic");
         Converter converter = new Converter("http://localhost:"+converterServer.getAddress().getPort(), "inCharacteristic", "outCharacteristic");
-        EventMathInterval events = new EventMathInterval(interval, "http://localhost:"+triggerServer.getAddress().getPort()+"/endpoint", "test", converter);
+        EventMathInterval events = new EventMathInterval("", interval, "http://localhost:"+triggerServer.getAddress().getPort()+"/endpoint", "test", converter);
         Config config = new Config(new JSONHelper().parseFile("config.json").toString());
         ConfigProvider.setConfig(config);
         MessageModel model = new MessageModel();
